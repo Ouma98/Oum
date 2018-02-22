6221aa6aac02836250658f52ee38fc97ee6541d0
LENOVO <LENOVO@WIN>
2018-02-22 13:10:02 +0000

    m

--- TestJSF/src/test/beans/BonjourBean.java
+++ TestJSF/src/test/beans/BonjourBean.java
@@ -7,17 +7,18 @@ import javax.faces.bean.RequestScoped;
 
 @ManagedBean
 @RequestScoped
-public class BonjourBean implements Serializable{
+public class BonjourBean implements Serializable {
 
-	   private static final long serialVersionUID = 1L;
+    // chi tkfita m3ak
+    private static final long serialVersionUID = 1L;
 
-	    private String nom;
+    private String nom;
 
-	    public String getNom() {
-	        return nom;
-	    }
+    public String getNom() {
+	return nom;
+    }
 
-	    public void setNom( String nom ) {
-	        this.nom = nom;
-	    }
+    public void setNom(String nom) {
+	this.nom = nom;
+    }
 }
